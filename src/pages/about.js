284934a3b88b4b0e1b498eed0042d9877f6b721c
okhuarobo osayuki <osayukiraymond@gmail.com>
2022-11-20 06:41:35 +0100

    added basic tags for testing

---
+++ src/pages/about.js
@@ -0,0 +1,13 @@
+import NavBar from "../components/NavBar";
+
+
+function AboutPage() {
+  return (
+    <div>
+      <NavBar/>
+        <h1>About Page</h1>
+    </div>
+  );
+}
+
+export default AboutPage;
