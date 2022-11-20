ba365cac5c9442c7cde7b2afa15ed94f5281e796
okhuarobo osayuki <osayukiraymond@gmail.com>
2022-11-20 06:39:20 +0100

    modified

---
+++ src/pages/home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "../assets/css/style.css";
+import NavBar from "../components/NavBar";
+
+function Home() {
+  return (
+    <>
+      <NavBar />
+    </>
+  );
+}
+
+export default Home;
