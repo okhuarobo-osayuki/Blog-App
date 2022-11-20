9f7a3b1f81556c1d3eabeb8e6ec17815b2a879cd
okhuarobo osayuki <osayukiraymond@gmail.com>
2022-11-20 23:17:25 +0100

    created a Navigation component for the whole app except homepage

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,7 +1,9 @@
+import { useState } from "react";
 import { Link, useMatch, useResolvedPath } from "react-router-dom";
 import "../assets/css/navbar.css";
 
-function CustomLInk({ to, children, ...props }) {
+// A custom link component that styles itself as active if the pathname of the URL matches the active link
+function CustomLink({ to, children, ...props }) {
   const resolvedPath = useResolvedPath(to);
   const isActive = useMatch({ path: resolvedPath.pathname });
 
@@ -14,20 +16,45 @@ function CustomLInk({ to, children, ...props }) {
   );
 }
 
+// The navigation component that contains the logo, the links to the different pages and the search functionality
 function NavBar() {
+  // An onclick function that is called when the search button is clicked and returns the search query; and also returns to normal state when the search button is clicked again
+  const [search, setSearch] = useState();
+  
+  // An onclick function that closes the search bar when the close button is clicked
+  const searchClose = () => {
+    setSearch();
+  };
+
+// An onclick function that opens the search bar when the search button is clicked
+  const searchClick = (e) => {
+    e.preventDefault();
+    setSearch((prev) => {
+      return (
+        <form className="search-form" method="GET" >
+          <input type="search" placeholder="Search for..." className="header-search-input" />
+          {/* close search button */}
+          <button className="search-close" onClick={searchClose}>close</button>
+        </form>
+      );
+    });
+  };
+
   return (
     <nav className="nav-bar">
       <h1>
         <Link to="/">Blog</Link>
       </h1>
       <ul>
-        <CustomLInk to="/">Home</CustomLInk>
-        <CustomLInk to="/pages/about">About</CustomLInk>
-        <CustomLInk to="/pages/">About</CustomLInk>
-        <CustomLInk to="/pages/">About</CustomLInk>
-        <CustomLInk to="/pages/">About</CustomLInk>
+        <CustomLink to="/">Home</CustomLink>
+        <CustomLink to="/pages/">Category</CustomLink>
+        <CustomLink to="/pages/">Blog</CustomLink>
+        <CustomLink to="/pages/about">About</CustomLink>
+        <CustomLink to="/pages/">Contact</CustomLink>
       </ul>
-      <div className="nav-search">
+      {/* Search value placed above the search icon */}
+        <>{search}</>
+      <div className="nav-search" onClick={searchClick}>
         <svg width="24" height="24" fill="none" viewBox="0 0 24 24">
           <path
             stroke="currentColor"
