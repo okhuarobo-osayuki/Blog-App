e82f90046e9ec165bbb6aa381bb8b1cc86713720
okhuarobo osayuki <osayukiraymond@gmail.com>
2022-12-09 17:29:52 +0100

    added footer component

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,1 +1,12 @@
 import React from "react";
+import "../assets/css/footer.css";
+
+function Footer(){
+    return (
+        <footer className="footer">
+
+        </footer>
+    );
+}
+
+export default Footer;
